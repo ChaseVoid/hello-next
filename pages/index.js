041d6bb65ddf2e3874e4c2441dbed37ae29c5dcd
Ajay Solleti <144334+ChaseVoid@users.noreply.github.com>
2020-04-05 03:09:43 +0530

    create dynamic pages
    * create dynamic pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
 import withLayout from '../components/Layout';
+import Link from 'next/link';
 
-const Page = () => <p>Hello Next.js</p>;
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+
+const Content = () => (
+  <>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js" />
+      <PostLink title="Learn Next.js is awesome" />
+      <PostLink title="Deploy apps with Zeit" />
+    </ul>
+  </>
+);
+
+const Page = () => <Content />;
 
 export default withLayout(Page);
